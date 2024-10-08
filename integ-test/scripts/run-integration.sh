060b64f715761a0c7e2991d8e7f0fa08bbc47653
Aman Gautam <amangautam2128@gmail.com>
2024-10-08 22:37:56 +0530

    Fixed bugs

--- integ-test/scripts/run-integration.sh
+++ integ-test/scripts/run-integration.sh
@@ -1,6 +1,7 @@
 docker-compose up -d
 echo '🟡 - Waiting for database to be ready...'
-./integ-test/scripts/wait-for-it.sh "postgresql://postgres:mypass@localhost:5432/postgres" -- echo '🟢 - Database is ready!'
+/scripts/wait-for-it.sh "postgresql://postgres:mypass@localhost:5432/postgres" 
+echo '🟢 - Database is ready!'
 npx prisma migrate dev --name init
 npm run test
 docker-compose down
